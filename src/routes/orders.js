428ddfde2a6ca5c5a799f897850250d7e8b0bdc2
JESSICA JINNETH ESCAMILLA CARRERO <jescamilla02@unisalle.edu.co>
2022-11-30 23:18:32 -0500

    add update logic

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,6 +2,7 @@ const express = require('express');
 const orderSchema = require('../models/order.js');
 const router = express.Router();
 const cors = require('cors');
+var moment = require('moment');
 
 router.use(cors({
     origin: 'http://192.168.3.105:3000'
@@ -9,10 +10,28 @@ router.use(cors({
 
 
 router.get('/orders',(req,res) =>{
-    console.log(req)
     orderSchema.find()
     .then((data) => res.json(data))
     .catch((error) => res.json({message:error}));
+})
+
+router.post('/orders',(req,res) =>{
+    orderSchema.findById(req.body.id)
+    .then((data) =>{
+        res.json(data)
+    })
+    .catch((error) => res.json({message:error}));;
+})
+
+router.post('/orders/update',(req,res) =>{
+    const temp = req.body;
+    console.log(temp)
+    orderSchema.updateOne(req.body)
+    .then((data) =>{
+        console.log(data)
+        res.json(data)
+    })
+    .catch((error) => res.json({message:error}));;
 })
 
 module.exports = router;
